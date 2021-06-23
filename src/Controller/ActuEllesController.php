28a7726f956794260d8a48bed987b68719db898b
Orijma <remymarquentin@gmail.com>
2021-06-23 10:20:26 +0200

    Just save

--- src/Controller/ActuEllesController.php
+++ src/Controller/ActuEllesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,14 @@ class ActuEllesController extends AbstractController
     /**
      * @Route("/actuelles", name="actu_elles")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('actu_elles/index.html.twig');
+        $articles = $articleRepository->findBy(
+            ['type' => 'citoyelle'],
+            ['date' => 'DESC']
+        );
+        return $this->render('actu_elles/index.html.twig', [
+            'articles' => $articles
+        ]);
     }
 }
