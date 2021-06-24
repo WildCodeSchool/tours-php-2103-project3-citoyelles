ac01b7dbd53304c5c6ada6a6fb27dc36b4152e8b
Amanel <amanel.gauriat@gmail.com>
2021-06-24 11:18:01 +0200

    members display

--- src/Controller/FestivEllesController.php
+++ src/Controller/FestivEllesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Member;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,13 @@ class FestivEllesController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('festiv_elles/index.html.twig');
+        $members = $this->getDoctrine()
+            ->getRepository(Member::class)
+            ->findBy([], ["title" => "ASC"]);
+        $membersOrdered = [];
+        foreach ($members as $member) {
+            $membersOrdered[$member->getTitle()][] = $member;
+        }
+        return $this->render('festiv_elles/index.html.twig', ["membersOrdered" => $membersOrdered]);
     }
 }
