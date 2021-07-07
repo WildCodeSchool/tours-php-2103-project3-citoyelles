d6fc8a84c43622e6e9b740f269adbaaa8acf0bda
arnaud-blanc <mail.github2@blaca.eu>
2021-07-07 11:19:58 +0200

    Upadte ArticleController, create src/Service/FileUploader

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,81 +4,134 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
+/**
+ * @Route("/article")
+ */
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/new_article", name="new_article")
+     * @Route("/new", name="new_article", methods={"GET","POST"})
      */
     public function new(
         Request $request,
         EntityManagerInterface $entityManager,
-        SluggerInterface $slugger
+        FileUploader $fileUploader
     ): Response {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
-        $articleRequest = $request->request->all();
 
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var UploadedFile $imageFile */
             $imageFile = $form->get('image')->getData();
             if ($imageFile !== null) {
-                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
-
-                try {
-                    $directory = $this->getParameter('images_directory');
-                    if (is_string($directory)) {
-                        $imageFile->move(
-                            $directory,
-                            $newFilename
-                        );
-                    } else {
-                        throw new HttpException(
-                            500,
-                            "Paramètres images_directory invalide, contacter les administrateurs du site"
-                        );
-                    }
-                } catch (FileException $e) {
-                    return $this->render('article/new.html.twig', [
-                        'form' => $form->createView(),
-                        'error' => "Une erreur a eu lieu lors de la création du fichier, veuillez réessayer."
-                    ]);
-                } catch (ServiceNotFoundException $e) {
-                    throw $e;
+                $imageFilename = $fileUploader->upload($imageFile);
+                if ($imageFilename === "") {
+                    $this->addFlash("error", "Impossible de récupérer le fichier, veuillez contacter l'administrateur");
+                    return $this->redirectToRoute('home'); // NON DÉFINITIF
                 }
-                $article->setImageFilename($newFilename);
+                $article->setImageFilename($imageFilename);
             }
             $entityManager->persist($article);
             $entityManager->flush();
 
             $this->addFlash('success', "L'article a bien été créé.");
-               switch ($articleRequest['article']['type']) {
-                case "festivelles":
-                     return $this->redirectToRoute('festivelles');
-                 case "citoyelles":
-                     return $this->redirectToRoute('actuelles');
-                 case "portrelles":
-                     return $this->redirectToRoute('actuelles');
-                 }
-            
+
+            switch ($article->getType()) {
+                case 'festivelles':
+                    return $this->redirectToRoute('festivelles');
+                case 'citoyelles':
+                    return $this->redirectToRoute('actuelles');
+                case 'portrelles':
+                    return $this->redirectToRoute('actuelles');
+            }
         }
 
         return $this->render('article/new.html.twig', [
             'form' => $form->createView(),
             "error" => ""
         ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit_article", requirements={"id"="\d+"}, methods={"GET","POST"})
+     */
+    public function edit(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        Article $article,
+        FileUploader $fileUploader
+    ): Response {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile !== null) {
+                $imageFilename = $fileUploader->upload($imageFile);
+                if ($imageFilename === "") {
+                    $this->addFlash("error", "Impossible de récupérer le fichier, veuillez contacter l'administrateur");
+                    return $this->redirectToRoute('home'); // NON DÉFINITIF
+                }
+                $fileUploader->delete($article->getImageFilename());
+                $article->setImageFilename($imageFilename);
+            }
+            $entityManager->flush();
+
+            $this->addFlash('success', "L'article a bien été modifié.");
+
+            switch ($article->getType()) {
+                case 'festivelles':
+                    return $this->redirectToRoute('festivelles');
+                case 'citoyelles':
+                    return $this->redirectToRoute('actuelles');
+                case 'portrelles':
+                    return $this->redirectToRoute('actuelles');
+            }
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_article", requirements={"id": "\d+"}, methods={"POST"})
+     */
+    public function delete(
+        Request $request,
+        Article $article,
+        FileUploader $fileUploader
+    ): Response {
+        $articleType = $article->getType();
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->request->get('_token'))) {
+            $fileUploader->delete($article->getImageFilename());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+
+            $this->addFlash('success', "L'article a bien été supprimé.");
+
+            switch ($articleType) {
+                case 'festivelles':
+                    return $this->redirectToRoute('festivelles');
+                case 'citoyelles':
+                    return $this->redirectToRoute('actuelles');
+                case 'portrelles':
+                    return $this->redirectToRoute('actuelles');
+            }
+        }
+        throw $this->createAccessDeniedException('Erreur 403 : Vous êtes en train de faire quelque chose d\'interdit.');
     }
 }
