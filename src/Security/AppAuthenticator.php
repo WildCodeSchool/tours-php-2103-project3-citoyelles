ad16da81f51aff492eec26a4c32e60054dcdb18e
Amanel <amanel.gauriat@gmail.com>
2021-07-07 10:54:35 +0200

    migration fix 2 and authentication redirect correction

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -102,7 +102,6 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
         }
 
         return new RedirectResponse($this->urlGenerator->generate('home'));
-        throw new Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
     protected function getLoginUrl()
